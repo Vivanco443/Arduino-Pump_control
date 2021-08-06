f9844a60d720a764ccde3eda63450150260b7c2c
Vivanco Qs <86332346+Vivanco443@users.noreply.github.com>
2021-08-05 22:52:50 -0500

    Adding a mechanical valve
    Becasuse we don´t have 2 elctrovalves anymore, otherwise just one now (the one that is between the first tank and the pump), we don´t need two sensors in the low tank, only the lower one.

--- waterPump_control_proyect.ino
+++ waterPump_control_proyect.ino
@@ -1,15 +1,14 @@
-
- // Cuando SAT1 y 0 estan e HIGH es que estan llenos
+// Cuando SAT1 y 0 estan e HIGH es que estan llenos
  // Cuando SBT1 y 0 estan e HIGH es que estan vacios
 
 //  Actuadores
 #define EB 3    //  enable bomba
 #define EV1 4 //  enable valvula 1
-#define EV0 5   //  enable valvula 0
+//Ya no hay electro valvula0 (#define EV0 5) 
 
   //  Sensores
 #define energia1 7    // Señal para los sensores
-#define SAT0 8    //  Sensor alto Tinaco 0 o tinaco de abajo
+// Ya no se necesita este sensor  (#define SAT0 8 )
 #define SBT0 9    //  Sensor bajo Tinaco 0 o tinaco de abajo
 #define SAT1 10   //  Sensor alto Tinaco 1 o tinaco de arriba
 #define SBT1 11   //  Sensor bajo Tinaco 1 o tinaco de arriba
@@ -17,22 +16,28 @@
 
 //banderas
 bool Bajotinaco000 = false;
-bool Altotinaco000 = false;
+// Ya no se necesita esta bandera (bool Altotinaco000 = false;)
 bool Bajotinaco111 = false;
 bool Altotinaco111 = false;
 
+//Variable de tiempo
+#define tiempoLlenado 3600 // El valor en segundos seria de 2n
+#define tiempoBomba 10800 // El valor en segundos seria s = n
+
+
 void setup() {
   pinMode(EB, OUTPUT);
-  pinMode(EV0, OUTPUT);
+//  pinMode(EV0, OUTPUT);
   pinMode(EV1, OUTPUT);
   digitalWrite(EB, LOW);
-  digitalWrite(EV0, LOW);
+//  digitalWrite(EV0, LOW);
   digitalWrite(EV1, LOW);
 
   pinMode(energia0, OUTPUT);
   pinMode(energia1, OUTPUT);
   digitalWrite(energia0, HIGH);
   digitalWrite(energia1, HIGH);
+  
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   
@@ -42,19 +47,25 @@ void setup() {
 void loop() {
 
   Bajotinaco111 = digitalRead(SBT1);
-  if (Bajotinaco111 == true){   // Esta abierto cuando hay agua   ; cuando falta el agua arriba
+  if (Bajotinaco111 == true){   // Esta abierto cuando no hay agua   ; cuando falta el agua arriba
     //  Osea que falta agua arriba
     
     do{   // Mientras se llena arriba
         Bajotinaco000 = digitalRead(SBT0);
-        
         if(Bajotinaco000 == false){    // Si hay agua en el tinaco de abajo
+            
             Elevacion_de_agua();
+        
         }
-        Llenado_tinaco_0();
-      
+
         Altotinaco111 = digitalRead(SAT1);
-    
+
+        if(Altotinaco111 == false){
+          
+              Espera();
+        }
+                      
+            
     } while(Altotinaco111 == false);
     
   }
@@ -65,43 +76,8 @@ void loop() {
 }
 
 
-void Llenado_tinaco_0(){
-  int i = 0;
-  digitalWrite(EV0, HIGH);    //Abrimos la valvula de llenado
-  do{
-          digitalWrite(LED_BUILTIN, HIGH);      // Mientras esta abierta dejamos parpadeando el led
-          delay(2000);                       
-          digitalWrite(LED_BUILTIN, LOW);    
-          delay(500);
-          digitalWrite(LED_BUILTIN, HIGH);   
-          delay(500);                       
-          digitalWrite(LED_BUILTIN, LOW);    
-          delay(500);
-          digitalWrite(LED_BUILTIN, HIGH);   
-          delay(500); 
-          digitalWrite(LED_BUILTIN, LOW);    
-          delay(500); 
-          Altotinaco000 = digitalRead(SAT0);
-          i++;
-          if(i >= 1200){  // Si ya pasaron 1.5 horas y no se llena
-            int j = 0;
-            digitalWrite(EV0, LOW); //  Apaga la valvula
-            while(j < 1500){  // La descansa una hora
-              digitalWrite(LED_BUILTIN, HIGH);
-              delay(400);
-              digitalWrite(LED_BUILTIN, LOW);
-              delay(2000);
-              j++;
-            }
-            i = 0;
-            digitalWrite(EV0, HIGH);    // La vuelve a encender
-          }
-  }while(Altotinaco000 == false);
-      
-  digitalWrite(EV0, LOW);   // Cerramos valvula de llenado
-}
-
 void Elevacion_de_agua(){
+  int i = 0;
   //Abrimos valvula 1
   digitalWrite(EV1, HIGH);
   delay(2000);
@@ -116,7 +92,10 @@ void Elevacion_de_agua(){
             
     // leemos sensores
     Bajotinaco000 = digitalRead(SBT0);
-    Altotinaco111 = digitalRead(SAT1); 
+    Altotinaco111 = digitalRead(SAT1);
+
+    i++;
+    if (i > tiempoBomba) break;
           
   }while(Bajotinaco000 == false && Altotinaco111 == false);
         
@@ -126,3 +105,20 @@ void Elevacion_de_agua(){
   digitalWrite(EV1, LOW);
 }
 
+
+void Espera(){
+  for(int j=0; j< tiempoLlenado; j++){
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(1000);                             //  Mientras se llena solo el 
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(200);                              
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(200);
+    digitalWrite(LED_BUILTIN, HIGH);   
+    delay(200);                             
+    digitalWrite(LED_BUILTIN, LOW);    
+    delay(200);
+  }
+}
